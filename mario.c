a3568713cb430cead7df9f65d404e6658ec70e2c
abanoubmesiha <abanoubmesiha@users.noreply.github.com>
2020-04-14 19:13:48 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

---
+++ mario.c
@@ -0,0 +1,35 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int number;
+    do
+    {
+        number = get_int("Height: ");
+    }
+    while (number < 1 || number > 8);
+    for (int line = 0; line < number; line++)
+    { 
+        for (int space = number - line - 1; space > 0; space--)
+        {
+            // add space to move pyramid to right.
+            printf(" ");
+        }
+        for (int width = 0; width <= line; width++)
+        {
+            // add pyramid blocks.
+            printf("#");
+        }
+        // add gab
+        printf("  ");
+        // add the second pyramid.
+        for (int width = 0; width <= line; width++)
+        {
+            // add pyramid blocks.
+            printf("#");
+        }
+        // add line after each row.
+        printf("\n");
+    }
+}
