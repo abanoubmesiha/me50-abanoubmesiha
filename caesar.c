d8719437f8d19e08807656f7278a24b99122850b
abanoubmesiha <abanoubmesiha@users.noreply.github.com>
2020-07-08 20:46:44 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -1,9 +1,3 @@
-// get key
-    // if not one integer
-        // throw error and return 1
-// make it number
-// get plain text
-// get cipher text
 #include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,36 +12,46 @@ int main(int argc, string argv[])
         return 1;
     }
     string key = argv[1];
+    // if not one integer
+    // throw error and return 1
     for (int i = 0; i < strlen(key); i++)
     {
         char c = key[i];
-        if (!(c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' ))
+        if (!(c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9'))
         {
             printf("Usage: ./caesar key\n");
             return 1;
         }
     }
+    // Get the key.
     int checkedKey = atoi(key);
     string plaintext = get_string("plaintext: ");
     printf("key: %i\n", checkedKey);
     printf("plaintext: %s\n", plaintext);
     printf("ciphertext: ");
 
+    // Make it number and get cipher text.
     for (int i = 0; i < strlen(plaintext); i++)
     {
         int ASCII = plaintext[i];
-        if ( ASCII >= 97 && ASCII <= 122)
+        if (ASCII >= 97 && ASCII <= 122)
         {
-            int c = ((ASCII-96) + checkedKey) % 26;
+            // Small letters
+            int c = ((ASCII - 96) + checkedKey) % 26;
             // ciphertext[i] = c + 96;
-            printf("%c",c + 96);
-        } else if (ASCII >= 64 && ASCII <= 90) {
-            int c = ((ASCII-63) + checkedKey) % 26;
-            printf("%c",c + 63);
-        } else {
-            printf("%c",ASCII);
+            printf("%c", c + 96);
+        }
+        else if (ASCII >= 64 && ASCII <= 90)
+        {
+            // Capital letters
+            int c = ((ASCII - 63) + checkedKey) % 26;
+            printf("%c", c + 63);
+        }
+        else
+        {
+            // Print anything else as it is.
+            printf("%c", ASCII);
         }
     }
     printf("\n");
-
 }
