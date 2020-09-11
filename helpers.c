0e6d458c20ac85f188cd7425e8c704d87c6f9a3e
abanoubmesiha <abanoubmesiha@users.noreply.github.com>
2020-09-11 19:08:04 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -18,10 +18,46 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     }
     return;
 }
-
+int betweenLimits(int value)
+{
+    int result;
+    if (value < 0){
+        result = 0;
+    }
+    else if (value > 255)
+    {
+        result =  255;
+    }
+    else
+    {
+        result = value;
+    }
+    return result;
+}
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int h=0; h<height; h++)
+    {
+        for (int w=0; w<width; w++)
+        {
+            int Red = image[h][w].rgbtRed;
+            int Green = image[h][w].rgbtGreen;
+            int Blue = image[h][w].rgbtBlue;
+
+            float sepiaRed = 0.393 * Red + 0.769 * Green + 0.189 * Blue;
+            float sepiaGreen = 0.349 * Red + 0.686 * Green + 0.168 * Blue;
+            float sepiaBlue = 0.272 * Red + 0.534 * Green + 0.131 * Blue;
+
+            Red = betweenLimits(round(sepiaRed));
+            Green = betweenLimits(round(sepiaGreen));
+            Blue = betweenLimits(round(sepiaBlue));
+
+            image[h][w].rgbtRed = Red;
+            image[h][w].rgbtGreen = Green;
+            image[h][w].rgbtBlue = Blue;
+        }
+    }
     return;
 }
 
