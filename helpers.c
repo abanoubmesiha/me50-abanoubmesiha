c5af7ed8b52b65506019236994494e9e8c9c2e80
abanoubmesiha <abanoubmesiha@users.noreply.github.com>
2020-09-11 20:32:38 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -60,12 +60,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     }
     return;
 }
-int copyValue(int value)
-{
-    int result;
-    result = value;
-    return result;
-}
+
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -73,38 +68,21 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int w=0; w<width; w++)
         {
-            // int firstRed = copyValue(image[h][w].rgbtRed);
-            // int firstGreen = copyValue(image[h][w].rgbtGreen);
-            // int firstBlue = copyValue(image[h][w].rgbtBlue);
+            int firstRed = image[h][w].rgbtRed;
+            int firstGreen = image[h][w].rgbtGreen;
+            int firstBlue = image[h][w].rgbtBlue;
 
-            // int secondRed = copyValue(image[h][width-1 -w].rgbtRed);
-            // int secondGreen = copyValue(image[h][width-1 -w].rgbtGreen);
-            // int secondBlue = copyValue(image[h][width-1 -w].rgbtBlue);
+            int secondRed = image[h][width-1 -w].rgbtRed;
+            int secondGreen = image[h][width-1 -w].rgbtGreen;
+            int secondBlue = image[h][width-1 -w].rgbtBlue;
 
-            // image[h][w].rgbtRed = secondRed;
-            // image[h][w].rgbtGreen = secondGreen;
-            // image[h][w].rgbtBlue = secondBlue;
+            image[h][w].rgbtRed = secondRed;
+            image[h][w].rgbtGreen = secondGreen;
+            image[h][w].rgbtBlue = secondBlue;
 
-            // image[h][width-1 -w].rgbtRed = firstRed;
-            // image[h][width-1 -w].rgbtGreen = firstGreen;
-            // image[h][width-1 -w].rgbtBlue = firstBlue;
-
-            // int firstRed = image[h][w].rgbtRed;
-            // int firstGreen = image[h][w].rgbtGreen;
-            // int firstBlue = image[h][w].rgbtBlue;
-
-            // int secondRed = image[h][width-1 -w].rgbtRed;
-            // int secondGreen = image[h][width-1 -w].rgbtGreen;
-            // int secondBlue = image[h][width-1 -w].rgbtBlue;
-
-            image[h][w].rgbtRed = image[h][width-1 -w].rgbtRed;
-            image[h][w].rgbtGreen = image[h][width-1 -w].rgbtGreen;
-            image[h][w].rgbtBlue = image[h][width-1 -w].rgbtBlue;
-
-            //  = firstRed;
-            //  = firstGreen;
-            //  = firstBlue;
-
+            image[h][width-1 -w].rgbtRed = firstRed;
+            image[h][width-1 -w].rgbtGreen = firstGreen;
+            image[h][width-1 -w].rgbtBlue = firstBlue;
         }
     }
     return;
