3f73c0115ec3825a51da743f904080ebf934493a
abanoubmesiha <abanoubmesiha@users.noreply.github.com>
2020-11-29 21:18:02 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -99,7 +99,48 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 }
 
 // Blur image
+struct pixel
+{
+    int red;
+    int green;
+    int blue;
+};
+void blurPixel(int h, int w, int height, int width, struct pixel img[height][width], struct pixel structName)
+{
+    // int squares = 9;
+    // struct pixel blured;
+    // calc red
+    // float total = img[h-1][w-1].red + img[h-1][w].red + img[h-1][w+1].red + img[h][w-1].red + img[h][w + 1].red + img[h+1][w-1].red + img[h+1][w].red + img[h-1][w+1].red;
+    // blured.red = round(total/squares);
+    structName.red = 1;
+    structName.green = 2;
+    structName.blue = 3;
+    return;
+}
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // copy the orinial image.
+    struct pixel img[height][width];
+    for (int h=0; h<height; h++)
+    {
+        for (int w=0; w<width; w++)
+        {
+            img[h][w].red = image[h][w].rgbtRed;
+            img[h][w].green = image[h][w].rgbtGreen;
+            img[h][w].blue = image[h][w].rgbtBlue;
+        }
+    }
+    // blur the original image.
+    for (int h=1; h<height-1; h++)
+    {
+        for (int w=1; w<width-1; w++)
+        {
+            // printf("Before: %i - ", image[h][w].rgbtRed);
+            image[h][w].rgbtRed = (img[h-1][w-1].red + img[h-1][w].red + img[h-1][w+1].red + img[h][w-1].red + img[h][w + 1].red + img[h+1][w-1].red + img[h+1][w].red + img[h-1][w+1].red) / 9;
+            image[h][w].rgbtGreen = (img[h-1][w-1].green + img[h-1][w].green + img[h-1][w+1].green + img[h][w-1].green + img[h][w + 1].green + img[h+1][w-1].green + img[h+1][w].green + img[h-1][w+1].green) / 9;
+            image[h][w].rgbtBlue = (img[h-1][w-1].blue + img[h-1][w].blue + img[h-1][w+1].blue + img[h][w-1].blue + img[h][w + 1].blue + img[h+1][w-1].blue + img[h+1][w].blue + img[h-1][w+1].blue) / 9;
+            // printf("After: %i\n", image[h][w].rgbtRed);
+        }
+    }
     return;
 }
