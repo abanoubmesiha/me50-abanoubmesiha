bc97d845ae13880d8b187554f5ae15229d460c18
abanoubmesiha <abanoubmesiha@users.noreply.github.com>
2020-12-20 20:29:24 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -6,15 +6,20 @@
 
 int main(int argc, string argv[])
 {
+    // check if card name didn't passed to the program
     if (argc < 2)
     {
         printf("Please, type the name of the card\n");
         return 1;
     }
+
     string card = argv[1];
-    // open memory card file
-    FILE* input = fopen(card, "r");
-    if (input == NULL)
+
+    // open card
+    FILE *data = fopen(card, "r");
+
+    // check if can't open
+    if (data == NULL)
     {
         printf("Could not open card.raw.\n");
         return 1;
@@ -23,47 +28,47 @@ int main(int argc, string argv[])
     unsigned char buffer[BUFFER_SIZE];
 
     // filename counter
-    int filecount = 0;
+    int count = 0;
 
-    FILE* picture = NULL;
+    FILE *pic = NULL;
 
-    // check if we've found a jpeg yet or not
+    // check if we've found a jpeg or not
     int jpg_found = 0; //false
 
-    // go through cardfile until there aren't any blocks left
-    while (fread(buffer, BUFFER_SIZE, 1, input) == 1)
+    // go through card until there aren't any data left
+    while (fread(buffer, BUFFER_SIZE, 1, data) == 1)
     {
         // read first 4 bytes of buffer and see if jpg signature using bitwise on last byte
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xe0) == 0xe0)
         {
             if (jpg_found == 1)
             {
-                // We found the start of a new pic so close out current picture
-                fclose(picture);
+                // We found the start of a new pic so we close out the current pic
+                fclose(pic);
             }
             else
             {
-                // jpg discovered and now we have the green light to write
+                // jpg discovered and now we start writing
                 jpg_found = 1;
             }
 
             char filename[8];
-            sprintf(filename, "%03d.jpg", filecount);
-            picture = fopen(filename, "a");
-            filecount++;
+            sprintf(filename, "%03d.jpg", count);
+            pic = fopen(filename, "a");
+            count++;
         }
 
         if (jpg_found == 1)
         {
-            // write 512 bytes to file once we start finding jpgs
-            fwrite(&buffer, BUFFER_SIZE, 1, picture);
+            // write 512 bytes to file when we start finding jpgs
+            fwrite(&buffer, BUFFER_SIZE, 1, pic);
         }
 
     }
 
     // close files
-    fclose(input);
-    fclose(picture);
+    fclose(data);
+    fclose(pic);
 
     return 0;
 }
