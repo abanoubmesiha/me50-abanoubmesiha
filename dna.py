e5f2d29c728ea58ff62724310654699ef62cc508
abanoubmesiha <abanoubmesiha@users.noreply.github.com>
2021-01-18 20:31:56 +0000

    automated commit by submit50

--- dna.py
+++ dna.py
@@ -9,6 +9,7 @@ csv_file = open(argv[1], 'r')
 
 strands = []
 persons = {}
+# Get Strands
 for ind, row in enumerate(csv_file):
     if ind == 0:
         strands = [strand for strand in row.strip().split(',')][1:]
@@ -18,24 +19,27 @@ for ind, row in enumerate(csv_file):
 
 dna_strand = open(argv[2], 'r').read()
 
+# Final Result
 final_strands = []
+
+# Loop over strands
 for strand in strands:
-    i = 0;
+    i = 0
     max_strand = -1
     cur_max = 0
     while i < len(dna_strand):
-        # Go
         cur_window = dna_strand[i:i + len(strand)]
         if cur_window == strand:
             cur_max += 1
             max_strand = max(max_strand, cur_max)
             i += len(strand)
         else:
-            cur_max = 0 # Reset cur_max
+            cur_max = 0  # Reset cur_max
             i += 1
     final_strands.append(max_strand)
 
-for name,data in persons.items():
+# Exit if found
+for name, data in persons.items():
     if data == final_strands:
         print(name)
         exit(0)
