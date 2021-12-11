c3873fa87f5b92b908adbc935cd2f25adbe7ce61
Abanoub Ayoub <abanoub@terkwaz.com>
2021-12-11 13:14:13 +0200

    Add links to entries list and view the entry when the user choose one of them.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def getEntry(req, title):
+    entry = util.get_entry(title)
+    if entry is None:
+        return render(req, "encyclopedia/404.html", { "title": title })
+    else:
+        return render(req, "encyclopedia/view-entry.html", { "entry": markdowner.convert(entry) })
+
+def search(req):
+    q = req.GET.get('q')
+    return HttpResponse(q)
